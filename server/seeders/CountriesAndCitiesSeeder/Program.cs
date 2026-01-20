d92b9a5915ee645dd7ececc70f4b05b5e2ea65cd
Vlad <vladoniscenko@outlook.com>
2026-01-20 20:12:21 +0100

    seeder and data

---
+++ server/seeders/CountriesAndCitiesSeeder/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Seeder;
+public class Program
+{
+    public static async Task Main()
+    {
+        Console.WriteLine("[SEEDER] Starting Country and City Seeder");
+        try
+        {
+            await CountriesAndCitiesSeerder.Seed();
+            Console.WriteLine("[SEEDER] Country and City Seeder Done");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[SEEDER] Error: {ex.Message}");
+        }
+    }
+}
