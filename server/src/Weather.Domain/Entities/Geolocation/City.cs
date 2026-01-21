fea12ba9c28334d8ed37638bfa7f1b47c55fcafa
Vlad <vladoniscenko@outlook.com>
2026-01-21 22:50:29 +0100

    update details method

--- server/src/Weather.Domain/Entities/Geolocation/City.cs
+++ server/src/Weather.Domain/Entities/Geolocation/City.cs
@@ -11,11 +11,11 @@ public class City : BaseEntity
     public long Population { get; private set; }
     public string Timezone { get; private set; }
 
-    public City(Guid countryId, string name, double latitude, double longitude, string timezone)
+    public City(Guid countryId, string name, double latitude, double longitude, string timezone, long population)
     {
         if (countryId == Guid.Empty) throw new ArgumentException("City must belong to a country.");
         if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("City Name is required.");
-        
+
         // Validate Coordinates
         if (latitude < -90 || latitude > 90) throw new ArgumentException("Invalid Latitude.");
         if (longitude < -180 || longitude > 180) throw new ArgumentException("Invalid Longitude.");
@@ -25,6 +25,19 @@ public class City : BaseEntity
         Latitude = latitude;
         Longitude = longitude;
         Timezone = timezone;
+        Population = population;
+    }
+
+    public void UpdateDetails(string name, long population, string timezone)
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("City Name is required.");
+        if (population < 0) throw new ArgumentException("Population cannot be negative.");
+
+        Name = name;
+        Population = population;
+        Timezone = timezone;
+
+        RegisterUpdate();
     }
 
     protected City() { } // for EF
