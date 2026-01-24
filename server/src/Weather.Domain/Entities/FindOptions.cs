5885feea87b25a55e7cb236e5a88db74d1ec737e
Vlad <vladoniscenko@outlook.com>
2026-01-24 22:59:18 +0100

    made find options generic and added more fields for filtering

--- server/src/Weather.Domain/Entities/FindOptions.cs
+++ server/src/Weather.Domain/Entities/FindOptions.cs
@@ -1,7 +1,10 @@
 namespace Weather.Domain.Entities;
 
-public class FindOptions
+public class FindOptions<TEntity>
 {
     public bool IsIgnoreAutoIncludes { get; set; }
     public bool IsAsNoTracking { get; set; } = true;
+    public int? Take { get; set; } = null;
+    public int? Page { get; set; } = null;
+    public Func<IQueryable<TEntity>, IQueryable<TEntity>>? OrderBy { get; set; }
 }
