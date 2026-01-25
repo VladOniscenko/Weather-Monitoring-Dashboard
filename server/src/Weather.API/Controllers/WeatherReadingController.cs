d30f3d953c1bc053e08b4654aed4cd70f520a4be
Vlad <vladoniscenko@outlook.com>
2026-01-25 16:37:42 +0100

    readings controller

---
+++ server/src/Weather.API/Controllers/WeatherReadingController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Weather.Domain.Entities;
+using Weather.Application.Common.Interfaces;
+using Weather.Application.Common.DTOs;
+using Weather.Application.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace Weather.API.Controllers;
+
+[ApiController]
+[Authorize(Roles = "Admin")]
+[Route("api/[controller]")]
+public class WeatherReadingController : BaseController
+{
+    private readonly IWeatherReadingService _service;
+    public WeatherReadingController(IWeatherReadingService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetAll([FromQuery] ReadingQuery? query = null)
+    {
+        var cities = await _service.QueryAsync(query);
+        return OkResponse(cities);
+    }
+
+    [HttpGet("{id}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var city = await _service.FindOneAsync(x => x.Id == id);
+        if (city == null)
+            return NotFoundResponse("Reading not found");
+
+        return OkResponse(city);
+    }
+}
