aa1937b79690da7ffd55c6f3c2fb3f7a906884aa
Vlad <vladoniscenko@outlook.com>
2026-01-25 16:37:59 +0100

    new method

--- server/src/Weather.Application/Common/Interfaces/IWeatherReadingService.cs
+++ server/src/Weather.Application/Common/Interfaces/IWeatherReadingService.cs
@@ -1,8 +1,9 @@
 using Weather.Domain.Entities;
+using Weather.Application.Common.DTOs;
 
 namespace Weather.Application.Common.Interfaces;
 
 public interface IWeatherReadingService : IGenericService<WeatherReading>
 {
-    
+    public Task<List<WeatherReading>> QueryAsync(ReadingQuery? query = null);
 }
--- server/src/Weather.Application/Services/WeatherReadingService.cs
+++ server/src/Weather.Application/Services/WeatherReadingService.cs
@@ -1,9 +1,55 @@
 using Weather.Domain.Entities;
 using Weather.Application.Common.Interfaces;
+using Weather.Application.Common.DTOs;
+using System.Linq.Expressions;
 
 namespace Weather.Application.Services;
 
-public class WeatherReadingService : GenericService<WeatherReading> , IWeatherReadingService
+public class WeatherReadingService : GenericService<WeatherReading>, IWeatherReadingService
 {
     public WeatherReadingService(IWeatherReadingRepository repo) : base(repo) { }
+
+    public async Task<List<WeatherReading>> QueryAsync(ReadingQuery? query = null)
+    {
+        query ??= new ReadingQuery();
+
+        Expression<Func<WeatherReading, bool>> predicate = ws => true;
+
+        if (query.StationId.HasValue)
+            predicate = Combine(predicate, ws => ws.StationId == query.StationId.Value);
+
+        if (query.Start.HasValue && !query.End.HasValue)
+        {
+            var dayStart = query.Start.Value.Date;
+            var dayEnd = dayStart.AddDays(1);
+
+            predicate = Combine(predicate,
+                ws => ws.CreatedAt >= dayStart && ws.CreatedAt < dayEnd);
+        }
+        else if (query.Start.HasValue && query.End.HasValue)
+        {
+            var start = query.Start.Value;
+            var end = query.End.Value;
+
+            predicate = Combine(predicate,
+                ws => ws.CreatedAt >= start && ws.CreatedAt <= end);
+        }
+
+
+        return await _repo.FindAsync(
+            predicate,
+            new FindOptions<WeatherReading> { Page = query.Page - 1, Take = query.PageSize }
+        );
+    }
+
+    private Expression<Func<WeatherReading, bool>> Combine(Expression<Func<WeatherReading, bool>> exp1, Expression<Func<WeatherReading, bool>> exp2)
+    {
+        var param = Expression.Parameter(typeof(WeatherReading), "ws");
+        var body = Expression.AndAlso(
+            Expression.Invoke(exp1, param),
+            Expression.Invoke(exp2, param)
+        );
+
+        return Expression.Lambda<Func<WeatherReading, bool>>(body, param);
+    }
 }
