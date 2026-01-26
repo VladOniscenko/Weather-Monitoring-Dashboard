1912703ac403acf8e7a311e96ae43bc29b71963e
Vlad <vladoniscenko@outlook.com>
2026-01-26 23:11:39 +0100

    added new query fields to station

--- server/src/Weather.Application/Common/DTOs/StationDtos.cs
+++ server/src/Weather.Application/Common/DTOs/StationDtos.cs
@@ -5,12 +5,17 @@ public class StationQuery
     public Guid? CityId { get; set; }
     public string? Name { get; set; }
 
+    public double? MinLng { get; set; }
+    public double? MaxLng { get; set; }
+    public double? MinLat { get; set; }
+    public double? MaxLat { get; set; }
+    public int? Zoom { get; set; }
 
     public int Page { get; init; } = 1;
     public int PageSize
     {
         get => _pageSize;
-        init => _pageSize = (value > 100) ? 100 : value;
+        init => _pageSize = (value > 1000) ? 1000 : value;
     }
-    private readonly int _pageSize = 20;
+    private readonly int _pageSize = 100;
 }
