690faabeab882ab453611c553f327cb74bbec78c
Vlad <vladoniscenko@outlook.com>
2026-01-26 23:14:38 +0100

    adjust query

--- server/src/Weather.Application/Common/DTOs/StationDtos.cs
+++ server/src/Weather.Application/Common/DTOs/StationDtos.cs
@@ -11,11 +11,11 @@ public class StationQuery
     public double? MaxLat { get; set; }
     public int? Zoom { get; set; }
 
-    public int Page { get; init; } = 1;
+    public int Page { get; init; } = 0;
     public int PageSize
     {
         get => _pageSize;
         init => _pageSize = (value > 1000) ? 1000 : value;
     }
-    private readonly int _pageSize = 100;
+    private readonly int _pageSize = 1000;
 }
