be6ec41d2bc34dd15e5d6ed1c7e7e3583c72be6c
Vlad <vladoniscenko@outlook.com>
2026-01-27 16:10:18 +0100

    get user geolocation

---
+++ client/src/hooks/useGeoLocation.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+interface Position {
+  lat: number;
+  lng: number;
+}
+
+export const useGeoLocation = () => {
+  const [position, setPosition] = useState<Position | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError('Geolocation is not supported by your browser.');
+      return;
+    }
+
+    const success = (pos: GeolocationPosition) => {
+      const { latitude, longitude } = pos.coords;
+      setPosition({ lat: latitude, lng: longitude });
+    };
+
+    const fail = (err: GeolocationPositionError) => {
+      console.log(err);
+      setPosition({ lat: 0, lng: 0 });
+      // switch (err.code) {
+      //   case err.PERMISSION_DENIED:
+      //     setError('User denied the request for Geolocation.');
+      //     break;
+      //   case err.POSITION_UNAVAILABLE:
+      //     setError('Location information is unavailable.');
+      //     break;
+      //   case err.TIMEOUT:
+      //     setError('The request to get user location timed out.');
+      //     break;
+      //   default:
+      //     setError('An unknown error occurred.');
+      //     break;
+      // }
+    };
+
+    navigator.geolocation.getCurrentPosition(success, fail);
+  }, []);
+
+  return { position, error };
+};
