f42af436de981dcede09909b53606315a2a901e5
Vlad <vladoniscenko@outlook.com>
2026-01-28 20:03:36 +0100

    renamed map

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { StationList } from '@/features/weather/components/StationList';
-import MapLayout from '@/features/weather/components/MapLayout';
+import Map from '@/features/weather/components/Map';
 // import { LoginPage } from '@/features/auth/pages/LoginPage';
 
 export const AppRouter = () => {
@@ -9,7 +9,7 @@ export const AppRouter = () => {
       {/* Public Routes */}
       <Route path="/" element={<>Home</>} />
       <Route path="/stations" element={<StationList />} />
-      <Route path="/map" element={<MapLayout/>} />
+      <Route path="/map" element={<Map/>} />
       
       {/* Auth Routes */}
       {/* <Route path="/login" element={<LoginPage />} /> */}
