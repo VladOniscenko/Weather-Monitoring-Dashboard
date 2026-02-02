0dee196f7cbc4c72d903554399e4c598b4404ec2
Vlad <vladoniscenko@outlook.com>
2026-02-02 20:22:24 +0100

    renamed to map

--- client/src/features/weather/components/Map.tsx
+++ client/src/features/weather/components/Map.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { type WeatherStationDto } from '@/client';
 
-import MapView from './MapView.tsx';
-import StatioDetails from './StationDetails.tsx';
+import MapView from './MapView/MapView.tsx';
+import StatioDetails from './StationDetails/StationDetails.tsx';
 
 const Map: React.FC = () => {
     const [open, setOpen] = useState<boolean>(false);
