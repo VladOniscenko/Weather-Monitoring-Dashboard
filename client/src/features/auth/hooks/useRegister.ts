f4c4d3034630b99322cf981e1802c97d9b870ca0
Vlad <vladoniscenko@outlook.com>
2026-02-06 18:33:57 +0100

    register hook

---
+++ client/src/features/auth/hooks/useRegister.ts
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+    validateEmail,
+    validateFullName,
+    validatePassword,
+} from '../utils/utils';
+import { UsersService } from '@/client';
+
+export const useRegister = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
+
+    const register = async (name: string, email: string, password: string) => {
+        setIsLoading(true);
+        setError(null);
+
+        const validations = [
+            validateFullName(name),
+            validateEmail(email),
+            validatePassword(password),
+        ];
+
+        const firstError = validations.find(Boolean);
+        if (firstError) {
+            throw new Error(firstError as string);
+        }
+
+        try {
+            const response = await UsersService.registerUser({
+                name,
+                email,
+                password,
+            });
+
+            if (!response.success) {
+                throw new Error(response.message || 'Registration failed');
+            }
+
+            navigate('/login');
+        } catch (err: any) {
+            setError(
+                err?.body?.message || err?.message || 'Something went wrong'
+            );
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { register, isLoading, error };
+};
