b09d8330dac8e1e0e20390226d00d50525349f58
Vlad <vladoniscenko@outlook.com>
2026-02-08 21:35:46 +0100

    reload stations on create

--- client/src/features/profile/components/index.tsx
+++ client/src/features/profile/components/index.tsx
@@ -1,13 +1,18 @@
 import UserDetails from '@/features/profile/components/UserDetails';
 import UserStations from '@/features/profile/components/UserStations';
 import StationCreateForm from './StationCreateForm';
+import { useState } from 'react';
 
 export const Profile = () => {
+    const [refreshKey, setRefreshKey] = useState(0);
+    function handleStationCreated() {
+        setRefreshKey((prev) => prev + 1);
+    }
     return (
-        <div className='p-4'>
-            <UserDetails/>
-            <StationCreateForm/>
-            <UserStations/>
+        <div className="p-4">
+            <UserDetails />
+            <StationCreateForm onCreated={handleStationCreated} />
+            <UserStations refreshKey={refreshKey} />
         </div>
     );
 };
