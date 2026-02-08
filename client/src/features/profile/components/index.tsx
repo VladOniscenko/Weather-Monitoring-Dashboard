4f18b1c3c5f1c72f014e81456394f3f15515f4f4
Vlad <vladoniscenko@outlook.com>
2026-02-08 16:23:35 +0100

    added wrapper

--- client/src/features/profile/components/index.tsx
+++ client/src/features/profile/components/index.tsx
@@ -1,11 +1,13 @@
 import UserDetails from '@/features/profile/components/UserDetails';
 import UserStations from '@/features/profile/components/UserStations';
+import StationCreateForm from './StationCreateForm';
 
 export const Profile = () => {
     return (
-        <>
+        <div className='p-4'>
             <UserDetails/>
+            <StationCreateForm/>
             <UserStations/>
-        </>
+        </div>
     );
 };
